7fe9581f2d39aba33f2f1680eb2e611ef84b359a
damho1 <sogu3032@gmail.com>
2024-11-15 02:13:51 +0900

    feat:  Analyzer의 요청에 따른 분석기 동작

--- spot/input/test.py
+++
@@ -1,8 +0,0 @@
-def hardcodedCredentialsExample():
-    # 비밀번호를 코드에 하드코딩: 보안 취약점
-    password = "my_secret_password"
-    print(f"Password: {password}")  # 비밀번호가 출력되어 노출됨
-
-hardcodedCredentialsExample()
-
-
